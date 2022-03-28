6f9de39ec09837d5dc820953039fe22ccdbc8107
zhaole <zhaole@crscd.com.cn>
2022-03-28 23:53:01 +0800

    using Netty to study how to read data

--- net-comm/src/main/java/com/crscd/cds/protocol/NettyClientHandler.java
+++ net-comm/src/main/java/com/crscd/cds/protocol/NettyClientHandler.java
@@ -3,6 +3,7 @@ package com.crscd.cds.protocol;
 
 import java.util.concurrent.TimeUnit;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.EventLoop;
 import io.netty.channel.SimpleChannelInboundHandler;
